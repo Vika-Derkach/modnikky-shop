09264e27deaeaa6edf561f46c41319e63ac3b257
Vika-Derkach <queenviktoriyphp@gmail.com>
2021-07-29 14:20:24 +0300

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log("app!");
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+const app = express();
+
+const PORT = config.get("port") || 5000;
+
+// app.listen(PORT, () => console.log("App has been started on the port "));
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Service is running on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+start();
